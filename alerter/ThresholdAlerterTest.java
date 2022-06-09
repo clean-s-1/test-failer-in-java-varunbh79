cc51625077680b2e3db8dd686cf2f781e58f71e4
Bharadwaj, Varun (ADV D AA PAMC NM DH1 ONG) <varun.bharadwaj@siemens.com>
2022-06-09 09:03:25 +0530

    Failure Alert Logic Change

--- alerter/ThresholdAlerterTest.java
+++ alerter/ThresholdAlerterTest.java
@@ -16,7 +16,7 @@ public class ThresholdAlerterTest {
             // let us keep a count of failures to report
             // However, this code doesn't count failures!
             // Add a test below to catch this bug. Alter the stub above, if needed.
-            alertFailureCount += 0;
+            alertFailureCount += 1;
             assert (alertFailureCount > 0):"Alert Failure Count Tracker Didn't Update";
         }
     }
